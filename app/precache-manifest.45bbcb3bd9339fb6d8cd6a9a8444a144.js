499af62846ecfbad911819ab0330e2d21595a803
da1fujimoto <da1fujimoto@gmail.com>
2020-02-21 10:54:52 +0900

    add app

---
+++ app/precache-manifest.45bbcb3bd9339fb6d8cd6a9a8444a144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddf14c837379e165a1e7e3ecf1890d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fdb2d39f7334a7aa9b13",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c81f21b5b880f7a1c817",
+    "url": "./static/js/2.704d9656.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.704d9656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb2d39f7334a7aa9b13",
+    "url": "./static/js/main.331a1a6d.chunk.js"
+  },
+  {
+    "revision": "735d59ab8c252f5aa172",
+    "url": "./static/js/runtime-main.e13758b4.js"
+  }
+]);
